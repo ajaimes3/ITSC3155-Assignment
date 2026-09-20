bc0d2e27351dd6b7679c8452bf763f614fcc28b6
Adrian <ajaimes3@charlotte.edu>
2026-09-20 18:33:13 -0400

    Complete Assignment 2 implementation

--- Assignment2/cashier.py
+++ Assignment2/cashier.py
@@ -6,8 +6,25 @@ class Cashier:
         """Returns the total calculated from coins inserted.
            Hint: include input() function here, e.g. input("how many quarters?: ")"""
         ###
+        print("Please insert coins.")
+        dollars = int(input("how many dollars?: ") or 0)
+        half_dollars = int(input("how many half dollars?: ") or 0)
+        quarters = int(input("how many quarters?: ") or 0)
+        nickels = int(input("how many nickels?: ") or 0)
+
+        total = (dollars * 1.0) + (half_dollars * 0.5) + (quarters * 0.25) + (nickels * 0.05)
+
+        return total
 
     def transaction_result(self, coins, cost):
         """Return True when the payment is accepted, or False if money is insufficient.
            Hint: use the output of process_coins() function for cost input"""
         ##
+        if coins >= cost:
+            change = round(coins - cost, 2)
+            if change > 0:
+                print(f"Here is ${change} in change.")
+            return True
+        else:
+            print("Sorry that's not enough money. Money refunded.")
+            return False
--- Assignment2/main.py
+++ Assignment2/main.py
@@ -6,14 +6,33 @@ from cashier import Cashier
 # Make an instance of other classes here
 resources = data.resources
 recipes = data.recipes
-sandwich_maker_instance = #####
-cashier_instance = ######
+sandwich_maker_instance = SandwichMaker(resources)#####
+cashier_instance = Cashier() ######
 
 
 
 
 def main():
     ###  write the rest of the codes ###
+    is_on = True
+
+    while is_on:
+        choice = input("What size sandwich would you like? (small, medium, large, off, report): ").lower()
+
+        if choice == "off":
+            is_on = False
+        elif choice == "report":
+            print(f"Bread: {sandwich_maker_instance.machine_resources['bread']} slices")
+            print(f"Ham: {sandwich_maker_instance.machine_resources['ham']} slices")
+            print(f"Cheese: {sandwich_maker_instance.machine_resources['cheese']} ounces")
+        elif choice in recipes:
+            sandwich_type = recipes[choice]
+            if sandwich_maker_instance.check_resources(sandwich_type["ingredients"]):
+                payment = cashier_instance.process_coins()
+                if cashier_instance.transaction_result(payment, sandwich_type["cost"]):
+                    sandwich_maker_instance.make_sandwich(choice, sandwich_type["ingredients"])
+        else:
+            print("Invalid choice. please select small, medium, large, off, or report.")
 
 if __name__=="__main__":
     main()
